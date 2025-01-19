8f74e2a17beaf9a3606a870dcc90dd9ce97f1253
mubashir1osmani <mubashir.osmani777@gmail.com>
2025-01-18 22:26:19 -0500

    longtiudnal model

---
+++ kinematic-model.py
@@ -0,0 +1,9 @@
+"""
+full vehicle modelling in 3d is complex
+longitudnal model is done when the vehicle moves uphill
+
+lateral model is done when the vehicle moves sideways
+
+kinematic model is a simplified model that assumes the vehicle moves in a straight line
+
+"""
